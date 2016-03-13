069f99ce10bae1a6e41eecba2d80be023c977bd8
jettairlines <lwanghe2@gmail.com>
2016-03-13 16:33:18 -0500

    render kinda works w/ key word

--- js/render.js
+++ js/render.js
@@ -1,47 +1,52 @@
+// code stolen from http://jsfiddle.net/tcfwH/304/
+
 var map;
+var infoWindow;
 
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 40.1077387, lng: -88.2286079},
-		zoom: 15
+function initialize() {
+    var myLatLng = new google.maps.LatLng(40.77, -73.97);
+    var myOptions = {
+          center: {lat: 40.1077387, lng: -88.2286079},
+          zoom: 15
+    };
+
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    data.forEach(function(e){
+	  makePolygon(e.cords, e.label);
 	});
+    
+}
 
-//				 map.addListener('click', addLatLng);
-
-
-
-	data.forEach(function(e){
-	  
-/*	  var marker = new MarkerWithLabel({
+function makePolygon(polyCoords, polyLabel) {
+    var marker = new MarkerWithLabel({
         position: new google.maps.LatLng(0,0),
         draggable: false,
         raiseOnDrag: false,
         map: map,
-        labelContent: e.label,
+        labelContent: polyLabel,
         labelAnchor: new google.maps.Point(30, 20),
         labelClass: "labels", // the CSS class for the label
         labelStyle: {opacity: 1.0},
         icon: "http://placehold.it/1x1",
         visible: false
-     });*/
-	  
-	  var temp = new google.maps.Polygon({
-	   paths: e.points,
-	   strokeWeight: 0,
-	   fillColor: '#FF0000',
-	   fillOpacity: 0.2
-	  });
+     });
 
-	  temp.setMap(map);
-	  
-	  google.maps.event.addListener(marker, "mousemove", function(event) {
-		marker.setPosition(event.latLng);
-		marker.setVisible(true);
-	  });
-	  google.maps.event.addListener(marker, "mouseout", function(event) {
-		marker.setVisible(false);
-	  });
-	});
-  
+    var poly = new google.maps.Polygon({
+        paths: polyCoords,
+        strokeWeight: 0,
+        fillColor: "#FF0000",
+        fillOpacity: 0.05,
+        map: map
+    });
 
+    google.maps.event.addListener(poly, "mousemove", function(event) {
+        marker.setPosition(event.latLng);
+        marker.setVisible(true);
+    });
+    google.maps.event.addListener(poly, "mouseout", function(event) {
+        marker.setVisible(false);
+    });
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
